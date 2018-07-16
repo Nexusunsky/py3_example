8924fcb96d97e577447e0d8879437c3640412e1c
Hao Liu <haoliu@thoughtworks.com>
2018-07-17 00:08:09 +0800

    Understanding Asynchronous IO With Python 3.4's Asyncio And Node.js

---
+++ cocurrent/asyncio/mock/yield_fib.py
@@ -0,0 +1,33 @@
+import sys
+
+from cocurrent.asyncio.mock.event_loop import EventLoop, print_every
+
+
+def fib(n):
+    if n <= 1:
+        yield n
+    else:
+        a = yield fib(n - 1)
+        b = yield fib(n - 2)
+        yield a + b
+
+
+def read_input(loop):
+    while True:
+        line = yield sys.stdin
+        n = int(line)
+        fib_n = yield fib(n)
+        print("fib({}) = {}".format(n, fib_n))
+
+
+def main():
+    loop = EventLoop()
+    hello_task = print_every('Hello world!', 3)
+    fib_task = read_input(loop)
+    loop.schedule(hello_task)
+    loop.schedule(fib_task)
+    loop.run_forever()
+
+
+if __name__ == '__main__':
+    main()
